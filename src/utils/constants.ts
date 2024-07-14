1458f012187872f526d790d80a4495ef7faa8381
e5lam el5ayed <eslam25elsayed12@gmail.com>
2024-07-15 01:26:17 +0300

    v

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 export const ARTICLE_PER_PAGE = 6;
 
-const PRODUCTION_DOMAIN = "http://localhost:3000";
+const PRODUCTION_DOMAIN = process.env.VERCEL_URL || "http://localhost:3000";
 const DEVELOPMENT_DOMAIN = "http://localhost:3000";
 
 export const DOMAIN = process.env.NODE_ENV === 'production' 
